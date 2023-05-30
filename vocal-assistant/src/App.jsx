e7fefa117f249b33d2d823f5c7d6a92ff25e559d
horenbergerb <horenbergerbeau@gmail.com>
2023-05-30 19:56:54 -0400

    forgot to add a file

--- vocal-assistant/src/App.jsx
+++ vocal-assistant/src/App.jsx
@@ -9,27 +9,16 @@ const App = () => {
     const [audioQueue, setAudioQueue] = useState([]);
     const [isPlaying, setIsPlaying] = useState(false);
 
-    let playbackQueue = Promise.resolve();
-
     const playAudio = async () => {
         if (audioQueue.length > 0) {
-            const audioUrl = audioQueue[0];
-            setAudioQueue(audioQueue.slice(1));
-    
-            // Create a new promise for the current audio and add it to the chain
-            playbackQueue = playbackQueue
-                .then(() => {
-                    return new Promise((resolve) => {
-                        const audio = new Audio(audioUrl);
-                        audio.onended = resolve;
-                        audio.play();
-                    });
-                })
-                .catch(console.error); // Log errors for easier debugging
+            setIsPlaying(true);
+            const audioUrl = audioQueue[0]; // Get the next audio URL without modifying the array
+            setAudioQueue(audioQueue.slice(1)); // Use slice to produce a new array without the first element
+            const audio = new Audio(audioUrl);
+            audio.onended = () => setIsPlaying(false);  // Play the next audio when this one ends
+            await audio.play();
         }
     }
-    
-    
 
     const onAudioReady = (data) => {
         const audioUrl = `http://localhost:5005/download/${data.filename}?cb=` + new Date().getTime();
